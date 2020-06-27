b535f46941904d36cbcc51b92de6ef688831edf1
yashraut362 <yashraut362@gmail.com>
2020-06-27 19:23:59 +0530

    request and res

--- Client-server/server.js
+++ Client-server/server.js
@@ -1,9 +1,45 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request recieved');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-type', 'text/html');
+
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path = path + 'index.html'
+            res.statusCode = 200;
+            break
+        case '/about':
+            path = path + 'about.html'
+            res.statusCode = 200;
+            break
+        case '/about-me':
+            res.setHeader('Location', '/about');
+            res.statusCode = 301;
+            res.end()
+            break
+        default:
+            path = path + '404.html'
+            res.statusCode = 404;
+            break
+    }
+    //send html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
-console.log('listening for request')
+    console.log('listening for request')
 })
