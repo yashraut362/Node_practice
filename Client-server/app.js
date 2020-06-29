e715e6fd3d43618efc5c0ddc31cb9145aa163d58
yashraut362 <yashraut362@gmail.com>
2020-06-29 21:44:04 +0530

    View engine and Ejs

--- Client-server/app.js
+++ Client-server/app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 
 const app = express();
+//regi view engine
+
+app.set('view engine', 'ejs');
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        { title: 'Yoshi finds egg', snippet: 'Lorem ipsum dolor knd owdg dsinsd ok nice' },
+        { title: 'Mario Hides egg', snippet: 'Lorem ipsum dolor knd owdg dsinsd ok nice' },
+        { title: 'Naruto Talk egg', snippet: 'Lorem ipsum dolor knd owdg dsinsd ok nice' },
+    ];
+    res.render('index', { title: "Home", blogs: blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: "About" })
 });
-
 
 app.get('/about-us', (req, res) => {
-    res.redirect('./about');
+    res.redirect('about');
 });
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: "Create a new blog" })
+})
+
 app.use((req, res) => {
-    res.status(404).sendfile('./views/404.html'), { root: __dirname }
+    res.status(404).render('404', { title: '404' });
 });
